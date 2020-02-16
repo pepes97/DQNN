c26bf55eb40c79a342a1891be17bbd10515b0156
MarcoPenne <marco.pennese.97@gmail.com>
2020-02-16 16:59:04 +0100

    added dowload script

---
+++ Dataset/download_dataset.py
@@ -0,0 +1,34 @@
+import urllib.request
+import os
+
+print('Beginning file download with urllib2...\n')
+
+url = ['https://zenodo.org/record/2580091/files/foa_dev.z01?download=1',
+       'https://zenodo.org/record/2580091/files/foa_dev.z02?download=1',
+       'https://zenodo.org/record/2580091/files/foa_dev.zip?download=1',
+       'https://zenodo.org/record/2580091/files/LICENSE?download=1',
+       'https://zenodo.org/record/2580091/files/metadata_dev.zip?download=1',
+       'https://zenodo.org/record/2580091/files/README.html?download=1']
+
+pre = 'https://zenodo.org/record/2580091/files/'
+post = '?download=1'
+files = os.listdir('.')
+for f in url:
+  name = f
+  name = name[len(pre):]
+  name = name[:-len(post)]
+  if name not in files:
+    print(f"Downloading: {name}")
+    urllib.request.urlretrieve(f, name)
+  else:
+    print(f"Already downloaded: {name}")
+
+import subprocess
+commands = [['zip', '-s', '0', 'foa_dev.zip', '--out', 'unsplit_foa_dev.zip'],
+            ['unzip', 'unsplit_foa_dev.zip'],
+            ['unzip', 'metadata_dev.zip']]
+
+for command in commands:
+    correct = subprocess.run(command,
+        # Probably don't forget these, too
+        check=True)
